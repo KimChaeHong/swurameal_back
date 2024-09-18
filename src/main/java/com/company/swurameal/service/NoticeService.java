f7e362ccb12ea1c80b34100188c4eb347d9f011c
gilju <giljurha93@gmail.com>
2024-09-18 21:00:57 +0900

    Add: 공지사항 Dao, Dto, Service 추가

---
+++ src/main/java/com/company/swurameal/service/NoticeService.java
@@ -0,0 +1,19 @@
+package com.company.swurameal.service;
+
+import java.util.List;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+import com.company.swurameal.dao.mybatis.NoticeDao;
+import com.company.swurameal.dto.NoticeDto;
+
+@Service
+public class NoticeService {
+	@Autowired
+	private NoticeDao noticeDao;
+	
+	public List<NoticeDto> getNotice() {
+		return noticeDao.selectAll();
+	}
+}
