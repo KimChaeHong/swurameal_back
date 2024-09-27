a34ce46e6c43ef028bded753990a22b03ce4e923
gilju <giljurha93@gmail.com>
2024-09-27 14:51:37 +0900

    Feat: 주문내역 Read 및 페이징 처리

--- src/main/java/com/company/swurameal/controller/OrderCotroller.java
+++ src/main/java/com/company/swurameal/controller/OrderCotroller.java
@@ -1,7 +1,18 @@
 package com.company.swurameal.controller;
 
+import java.util.List;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.security.access.annotation.Secured;
+import org.springframework.security.core.Authentication;
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.RequestMapping;
+
+import com.company.swurameal.dto.CartDto;
+import com.company.swurameal.dto.UserDto;
+import com.company.swurameal.sercurity.CustomUserDetails;
+import com.company.swurameal.service.CartService;
 
 import lombok.extern.slf4j.Slf4j;
 
@@ -10,10 +21,23 @@ import lombok.extern.slf4j.Slf4j;
 @RequestMapping("/order")
 public class OrderCotroller {
 	
+	@Autowired
+	private CartService cartService;
+	
+	@Secured("ROLE_USER")
 	@RequestMapping("/order")
-	public String order() {
-		log.info("주문");
-		return "order/order";
+	public String order(
+		Model model,
+		Authentication authentication
+			) {
+			CustomUserDetails userDetails = (CustomUserDetails) authentication.getPrincipal();
+			UserDto user = userDetails.getUserDto();
+			String userId = user.getUserId();
+			
+			List<CartDto> cartList = cartService.getGoods(userId);
+			
+			log.info("주문");
+			return "order/order";
 	}
 	
 }
