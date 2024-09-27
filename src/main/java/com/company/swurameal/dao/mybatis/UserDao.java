26f63da4b43cbbfabbdc7477707f03b549d5e08b
kimccchong <coghd28@naver.com>
2024-09-27 12:15:14 +0900

    💡 Add: "User 관련 기능에 주석 추가"

--- src/main/java/com/company/swurameal/dao/mybatis/UserDao.java
+++ src/main/java/com/company/swurameal/dao/mybatis/UserDao.java
@@ -7,16 +7,16 @@ import com.company.swurameal.dto.UserDto;
 @Mapper
 public interface UserDao {
 	
+	//회원 가입
 	public int insert(UserDto user);
-	public UserDto selectByUserId(String userId);
-<<<<<<< HEAD
 	
-=======
-	public boolean existsByUserId(String userId);
+	//회원 정보 가져오기
+	public UserDto selectByUserId(String userId);
+
 	//회원 정보 수정
 	public int updateByUserId(UserDto user);
+	
 	//회원 정보 비활성화
 	public int deactivateUserById(String userId);
 
->>>>>>> branch 'master' of https://github.com/KimChaeHong/swurameal_back
 }
--- src/main/java/com/company/swurameal/service/UserService.java
+++ src/main/java/com/company/swurameal/service/UserService.java
@@ -13,84 +13,80 @@ import lombok.extern.slf4j.Slf4j;
 @Slf4j
 public class UserService {
 
-	public enum JoinResult{
-		SUCCESS,
-		FAIL_DUPLICATED_USERID
-		
+	public enum JoinResult {
+		SUCCESS, FAIL_DUPLICATED_USERID
+
 	}
-	public enum LoginResult{
-		SUCCESS,
-		FAIL_USERID,
-		FAIL_USERPASSSWORD,
-		FAIL_ENABLED
-		
+
+	public enum LoginResult {
+		SUCCESS, FAIL_USERID, FAIL_USERPASSSWORD, FAIL_ENABLED
+
 	}
-	
+
 	@Resource
 	private UserDao userDao;
-	
+
+	// 아이디 중복 여부 검사
 	public JoinResult join(UserDto user) {
 		boolean exist = isUserid(user.getUserId());
 		if (exist) {
 			return JoinResult.FAIL_DUPLICATED_USERID;
 		}
-		
+
 		userDao.insert(user);
 		return JoinResult.SUCCESS;
-	
+
 	}
 
-	// 중복 체크를 위한 메서드
-    public boolean isUserid(String userId) {
-        UserDto user = userDao.selectByUserId(userId);
-        if (user == null) {
+	// 아이디 중복 검사를 위한 메서드
+	public boolean isUserid(String userId) {
+		UserDto user = userDao.selectByUserId(userId);
+		if (user == null) {
 			return false;
-		}else {
+		} else {
 			return true;
 		}
-    }
-	
+	}
+
+	// 로그인 결과값 반환 메서드
 	public LoginResult login(UserDto user) {
 		UserDto dbuser = userDao.selectByUserId(user.getUserId());
-		
-		//1. 로그인
-		if(dbuser == null) {
+
+		// 1. 로그인
+		if (dbuser == null) {
 			return LoginResult.FAIL_USERID;
 		}
-		
-		//2.아이디 존재 안함
-		if(!dbuser.isUserEnable()) {
+
+		// 2.아이디 존재 안함
+		if (!dbuser.isUserEnable()) {
 			return LoginResult.FAIL_ENABLED;
 		}
-		
-		//3. 비밀번호 틀림
-		if(!dbuser.getUserPw().equals(user.getUserPw())) {
+
+		// 3. 비밀번호 틀림
+		if (!dbuser.getUserPw().equals(user.getUserPw())) {
 			return LoginResult.FAIL_USERPASSSWORD;
 		}
-		
+
 		return LoginResult.SUCCESS;
 	}
 
-	// userId가 데이터베이스에 존재하는지 확인
-    public boolean isUserIdExists(String userId) {
-        return userDao.existsByUserId(userId);
-    }
-    
-    //유저 정보 수정
-    public int updateByUserId(UserDto user) {
-    	return userDao.updateByUserId(user);
-    }
+	// 유저 정보 수정
+	public int updateByUserId(UserDto user) {
+		return userDao.updateByUserId(user);
+	}
 
 	public UserDto getUserById(String userId) {
 		return userDao.selectByUserId(userId);
 	}
-	//유저 정보 조회
+
+	// 유저 정보 조회
 	public UserDto findUserById(String userId) {
 		return userDao.selectByUserId(userId);
 	}
-	//유저 정보 비활성화
+
+	// 유저 정보 비활성화
 	public int deactivateUserById(String userId) {
 		return userDao.deactivateUserById(userId);
-		
+
 	}
 }
